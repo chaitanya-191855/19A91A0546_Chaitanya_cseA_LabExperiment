80bead7b855368f96d553ca75e24d800f92bebf7
Raavi Venkata Sai Chaitanya <69502127+chaitanya-191855@users.noreply.github.com>
2021-06-12 10:31:32 +0530

    Update Ex_5.1)Data Structure.py

--- Ex_5.1)Data Structure.py
+++ Ex_5.1)Data Structure.py
@@ -1,7 +1,7 @@
 """
 5.1) Implement a python script to count frequency of characters in a given string.
 """
-s=input("enter the string")
+s=input("Enter the string :")
 result = {}
 for letter in s:
     if letter not in result:
@@ -10,5 +10,5 @@ for letter in s:
           result[letter.lower()] += 1
 print("count frequency of characters in given string:",result)
 #output
-enter the string hello world
+Enter the string :hello world
 count frequency of characters in given string: {'h': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
